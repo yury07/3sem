bad832a8b347277cf346fcd69618352cf360def2
yury Pavlyuk <yurypavlyuk1@MacBook-Pro-yury.local>
2015-10-01 13:30:52 +0300

    deadline 1.10.15, LOL

---
+++ line.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#define N 10
+
+int main()
+{
+    int pid, i, status;
+    
+    for(i = 0; i < N; i++)
+    {
+        pid = fork();
+        
+        sleep(2);
+        
+        if(pid == -1)
+        {
+            printf("ERROR!\n");
+        }
+        else if(pid == 0)
+        {
+            continue;
+        }
+        break;
+    }
+    printf("pid = %d, ppid = %d\n" , getpid(), getppid());
+    pid = wait(&status);
+    printf("Process %d exited with status %d and returned %d\n", pid, status & 255, (status & 65280) >> 8);
+    return getpid();
+}
