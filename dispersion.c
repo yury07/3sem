c595ef0d04c8987843488a1ec4ae5acdbf3a0fee
VladMiryaha <vlad.miryaha@gmail.com>
2015-10-15 23:35:20 +0300

    ok

--- dispersion.c
+++ dispersion.c
@@ -1,5 +1,11 @@
 /* не могу найти ошибку в вычисление дисперсии, помогите пожалуйста*/
 
+/*
+ * Вы время странно засекали для дисперсии, поэтому казалось, что на её вычисление тратится в 2-3 раза больше времени, чем на
+ * вычисление дисперсии.
+ * Результат вроде бы верный вычисляется, и вроде даже ускорение видно (с помощью утилиты time замерял)
+ * Засчитано.
+ */
 
 #include <pthread.h>
 #include <stdio.h>
@@ -7,8 +13,8 @@
 #include <string.h>
 #include <unistd.h>
 #include <time.h>
-#define N 10000000
-#define K 10
+#define N 100000000
+#define K 2
 
 struct Data{
     int size;
@@ -19,15 +25,14 @@ struct Data{
 void* sum(void*);
 void* dispersion(void* p);
 
-
 int main(){
     
     srand(time(NULL));
     double *arr = (double*)calloc(N, sizeof(double));
-    //double *aar = (double*)calloc(N, sizeof(double));
     
     for (int i = 0; i < N; i++) {
-        arr[i] = (double)rand() / RAND_MAX*(100) - 50;
+        //arr[i] = (double)rand() / RAND_MAX*(100) - 50;
+        arr[i] = rand() % 2;
     }
     
     struct Data *str = (struct Data*) calloc(K , sizeof(struct Data));
@@ -51,24 +56,22 @@ int main(){
     double av = 0;
     for (int i = 0; i < K; i++) {
         pthread_join(thread_id[i], NULL);
-        printf("* no %d, start %d, size %d, sum %lg, aver %lg\n", i, str[i].t, str[i].size, str[i].result, str[i].result/str[i].size);
+        printf("* no %d, start %p, size %d, sum %lg, aver %f\n", i, str[i].t, str[i].size, str[i].result, str[i].result / str[i].size);
         av += str[i].result;
-        str[i].result = 0;
     }
     
     av /= N;
     printf("Averange: %4.4lg\n", av);
-    unsigned int cl2 = -cl1 + clock();
-    printf ( "Averange time: %u\n", cl2);
+    printf ( "Averange time: %ld\n", clock() - cl1);
     
     for (int j = 0 ; j < K; j++) {
-        str[j].t = arr + j * N/K;
-        str[j].size = N/K;
         str[j].result = 0;
     }
     
+    unsigned int dispersionStartTime = clock();
+    
     for (int i = 0; i < K; i++) {
-        int result = pthread_create( &thread_id[i], (pthread_attr_t*) NULL, dispersion, (void*)&str[i]);
+        int result = pthread_create( &(thread_id[i]), (pthread_attr_t*) NULL, dispersion, (void*)&str[i]);
         if (result > 0){
             printf("Error");
             exit(0);
@@ -83,10 +86,9 @@ int main(){
     
     dis = dis / N - av * av;
     printf("Dispersion: %4.4lg\n", dis);
-    printf("Dispersion time: %u\n",clock() - cl2);
+    printf("Dispersion time: %ld\n",clock() - dispersionStartTime);
     
     free(arr);
-    //free(aar);
     free(str);
     
     return 0;
@@ -98,7 +100,7 @@ void* sum(void* p){
     int size = ((struct Data *)p) ->size;
     
     for( int i = 0; i < size; i++){
-        result += *(start + i);
+        result += start[i];
     }
     
     ((struct Data*)p)->result = result;
