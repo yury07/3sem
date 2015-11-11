2b076b5a449e20cc8b4d1f51661de10232abb7ba
yury <manyn2@yandex.ru>
2015-11-11 20:32:20 +0300

    12.11

---
+++ time.c
@@ -0,0 +1,35 @@
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+int t;
+void* my_handler(int nsig);
+
+
+int main(void){
+    signal(SIGALRM, my_handler);
+    while(1){
+        scanf("%d", &t);
+        if(t < 0)
+        break;
+        alarm(1);
+    }
+    return 0;
+}
+
+
+
+
+void* my_handler(int nsig){
+    if (nsig == SIGALRM) {
+        if (t > 0){
+            t--;
+            printf("time: %d\a\n", t);
+            alarm(1);
+        }
+        else{
+            printf("\a\a\a\a\a\n");
+        }
+    }
+    
+    return NULL;
+}
